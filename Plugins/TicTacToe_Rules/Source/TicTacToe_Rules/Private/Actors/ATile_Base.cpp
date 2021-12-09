31df4698215900af253b6af036b69f72c1fcfb46
NickTheMonkey <6wolf92@gmail.com>
2021-12-09 23:55:50 +0200

    -базовый тайл

---
+++ Plugins/TicTacToe_Rules/Source/TicTacToe_Rules/Private/Actors/ATile_Base.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/ATile_Base.h"
+
+// Sets default values
+AATile_Base::AATile_Base()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh->CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	// TODO: когда будет пофикшен баг со структурой FKey - включить эту функцию (нужна для реакции на клик мышки)
+	//Mesh->OnClicked.AddDynamic(this, &AATile_Base::OnClick);
+}
+
+// Called when the game starts or when spawned
+void AATile_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+/*void AATile_Base::OnClick_Implementation(UPrimitiveComponent* ClickedActor, FKey ButtonPressed)
+{
+	Event_Click();
+}*/
+
+// Called every frame
+void AATile_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AATile_Base::Event_Click()
+{
+	OnTileClicked.Execute(TileNumber);
+}
+
+void AATile_Base::SetTileNumber(const int32 Number)
+{
+	TileNumber = Number;
+}
+
+int32 AATile_Base::GetTileNumber()
+{
+	return TileNumber;
+}
+
+FVector AATile_Base::GetBounds()
+{
+	FVector meshBoundsMin;
+	FVector meshBoundsMax;
+	Mesh->GetLocalBounds(meshBoundsMin, meshBoundsMax);
+	meshBoundsMax -= meshBoundsMin; //получим реальный размер тайла (меша)
+	return meshBoundsMax;
+}
+
---
+++ Plugins/TicTacToe_Rules/Source/TicTacToe_Rules/Public/Actors/ATile_Base.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ATile_Base.generated.h"
+
+DECLARE_DELEGATE_OneParam(FOnTileClicked, int32);
+
+UCLASS()
+class TICTACTOE_RULES_API AATile_Base : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AATile_Base();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//TODO: пофиксить баг компил€ции при использовании структуры FKey
+	/*UFUNCTION(BlueprintNativeEvent)
+	void OnClick(UPrimitiveComponent* ClickedActor, FKey ButtonPressed);*/
+
+	UPROPERTY()
+	int32 TileNumber;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FOnTileClicked OnTileClicked;
+
+	UFUNCTION()
+	void Event_Click();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Mesh;
+
+	UFUNCTION(BlueprintCallable)
+	void SetTileNumber(const int32 Number);
+
+	UFUNCTION(BlueprintCallable)
+	int32 GetTileNumber();
+
+	UFUNCTION(BlueprintCallable)
+	FVector GetBounds();
+};
