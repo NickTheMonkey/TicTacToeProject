a4e5a9d69dad9b02d12901028cd8077a72298c9d
NickTheMonkey <6wolf92@gmail.com>
2021-12-09 16:24:14 +0200

    -Initialization

---
+++ Source/TicTacToeProject/TicTacToeProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TicTacToeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOEPROJECT_API ATicTacToeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
