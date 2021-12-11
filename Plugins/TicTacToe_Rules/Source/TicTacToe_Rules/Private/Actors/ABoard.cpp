65e432954032042e3c621aa5f812082e36abe8e8
NickTheMonkey <6wolf92@gmail.com>
2021-12-11 17:53:11 +0200

    -добавлены функции дял использования ИИ (GetBoardStatus для пересмотра доски и SetTileStatus для хода ИИ)

--- Plugins/TicTacToe_Rules/Source/TicTacToe_Rules/Private/Actors/ABoard.cpp
+++ Plugins/TicTacToe_Rules/Source/TicTacToe_Rules/Private/Actors/ABoard.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Actors/ABoard.h"
+
 #include "Actors/ATile_Base.h"
 #include "Components/AComp_BoardInfo.h"
 
@@ -20,11 +21,29 @@ void AABoard::BeginPlay()
 	Super::BeginPlay();
 }
 
+void AABoard::ClickOnTile(const int32& l_TileNumber, const PlayersSymbol& l_Symbol)
+{
+	
+}
+
 // Called every frame
 void AABoard::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AABoard::GetBoardStatus(TArray<PlayersSymbol>& l_Field)
+{
+	boardInfo->GetFieldStatus(l_Field);
+}
+
+void AABoard::SetTileStatus(const int32& l_TileNumber, const PlayersSymbol& l_NewSymbol)
+{
+	if(boardInfo->SetTileSymbolForNumber(l_TileNumber, l_NewSymbol))
+	{
+		Tiles[l_TileNumber]->SetSymbol(l_NewSymbol);
+	};
 }
 
 void AABoard::NewGame_Implementation(int32 field_Width, int32 field_Height, const int32& l_SymbolsInLineToWin)
@@ -33,6 +52,7 @@ void AABoard::NewGame_Implementation(int32 field_Width, int32 field_Height, cons
 	{
 		for(int32 a = 0; a < Tiles.Num(); a++)
 		{
+			Tiles[a]->OnTileClicked.Unbind();
 			Tiles[a]->Destroy();
 		}
 		Tiles.Empty();
@@ -43,26 +63,29 @@ void AABoard::NewGame_Implementation(int32 field_Width, int32 field_Height, cons
 		TArray<PlayersSymbol> l_field;
 		boardInfo->GetFieldStatus(l_field);
 		
-		const FVector l_Tile = TilesType.GetDefaultObject()->GetBounds();
-		const float l_DistanceBetween = DistanceBetweenTiles * .5f; //делится на два, потому что находящиеся рядом тайлы буду отступать друг от друга на заданное здесь расстояние. Если каждый будет отступать на указанное, дизайнером, расстояние, то промежуточное, фактически, будет вдвое больше
-		
+		const FVector l_Tile = TilesType.GetDefaultObject()->GetBounds(); // сохраняется размер тайла
+
+		// место, откуда начнут спауниться тайлы с рассчетом на то, чтоб рут этого эктора находился по центру сгенерированного поля
 		FVector spawnStart = { 0.f, 0.f, 0.f};
-		spawnStart.X = (static_cast<float>(field_Width - 1) * l_Tile.X * .5f + (static_cast<float>(field_Width - 1) * DistanceBetweenTiles)) * .5f;
-		spawnStart.Y = (static_cast<float>(field_Height - 1) * l_Tile.Y * .5f + (static_cast<float>(field_Height - 1) * DistanceBetweenTiles)) * .5f;
+		spawnStart.X = static_cast<float>(field_Width - 1) * l_Tile.X * .5f;
+		spawnStart.Y = static_cast<float>(field_Height - 1) * l_Tile.Y * .5f;
+		
 		if(l_field.Num() > 0)
 		{
 			for(int32 y = 0; y < field_Height; y++)
 			{
 				for(int32 x = 0; x < field_Width; x++)
 				{
-					//TODO: найти проблему, из-за которой тайлы генерируются немного правее (не постоянная величина)
 					FVector spawnLocation = GetActorLocation() - spawnStart;
-					spawnLocation.X += static_cast<float>(x) * l_Tile.X + static_cast<float>(x) * l_DistanceBetween;
-					spawnLocation.Y += static_cast<float>(y) * l_Tile.Y + static_cast<float>(y) * l_DistanceBetween;
+					spawnLocation.X += static_cast<float>(x) * l_Tile.X;
+					spawnLocation.Y += static_cast<float>(y) * l_Tile.Y;
+					
 					AATile_Base* createdTile = Cast<AATile_Base>(GetWorld()->SpawnActor(TilesType, &spawnLocation));
 					int32 l_Number;
 					if(boardInfo->Convert_Position_ToNumber(x,y,l_Number)) createdTile->SetTileNumber(l_Number);
 					Tiles.Add(createdTile);
+
+					//createdTile->OnTileClicked.BindUFunction(this, "ClickOnTile");
 				}
 			}
 		}
