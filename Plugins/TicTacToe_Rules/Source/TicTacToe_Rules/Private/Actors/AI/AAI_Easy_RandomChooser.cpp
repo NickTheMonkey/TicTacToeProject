99d3bc3de250b8504656b62c2f6dca0a17c75fde
NickTheMonkey <6wolf92@gmail.com>
2021-12-12 20:43:58 +0200

    -тип просто ИИ. Алгоритм: случаное свободное поле

---
+++ Plugins/TicTacToe_Rules/Source/TicTacToe_Rules/Private/Actors/AI/AAI_Easy_RandomChooser.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/AI/AAI_Easy_RandomChooser.h"
+
+int32 AAAI_Easy_RandomChooser::CalculateTurn(const TArray<PlayersSymbol>& board)
+{
+	if(board.Num() > 0)
+	{
+		TArray<int32> filtered;
+
+		// поиск свободных тайлов
+		for(int32 a = 0; a < board.Num(); a++)
+		{
+			if(board[a] == PlayersSymbol::PSymb_None)
+			{
+				filtered.Add(a);
+			}
+		}
+
+		int32 result = FMath::RandRange(0, filtered.Num() - 1); // выбор случайного элемента из массива выбранных пустых тайлов
+		if(filtered.IsValidIndex(result))
+		{
+			result = filtered[result];
+			return result;
+		}
+	}
+
+	return -1;
+}
---
+++ Plugins/TicTacToe_Rules/Source/TicTacToe_Rules/Public/Actors/AI/AAI_Easy_RandomChooser.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/AI/AAI_Base.h"
+#include "AAI_Easy_RandomChooser.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOE_RULES_API AAAI_Easy_RandomChooser : public AAAI_Base
+{
+	GENERATED_BODY()
+
+protected:
+	// функция, в которой будет находиться алгоритм рассчета хода
+	//UFUNCTION()
+	virtual int32 CalculateTurn(const TArray<PlayersSymbol>& board) override;
+};
